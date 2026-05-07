90fda3c162f8901e4b09417ee52134a4409a2f3c
Tomatillo1 <totofarkas@gmail.com>
2026-05-07 18:18:41 +0200

    pwa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,28 +4,28 @@
   Initialise les stores depuis localStorage au montage.
 -->
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { currentPage, todayEntries, weekHistory } from '$lib/stores';
-	import HomePage from '$lib/components/HomePage.svelte';
-	import HistoryPage from '$lib/components/HistoryPage.svelte';
-	import FoodsPage from '$lib/components/FoodsPage.svelte';
+    import { onMount } from "svelte";
+    import { currentPage, todayEntries, weekHistory } from "$lib/stores";
+    import HomePage from "$lib/components/HomePage.svelte";
+    import HistoryPage from "$lib/components/HistoryPage.svelte";
+    import FoodsPage from "$lib/components/FoodsPage.svelte";
 
-	/** Initialisation des stores depuis localStorage */
-	onMount(() => {
-		todayEntries.init();
-		weekHistory.init();
-	});
+    /** Initialisation des stores depuis localStorage */
+    onMount(() => {
+        todayEntries.init();
+        weekHistory.init();
+    });
 </script>
 
 <svelte:head>
-	<title>NutriTrack - Suivi Nutritionnel</title>
+    <title>NutriTrack</title>
 </svelte:head>
 
 <!-- Affichage conditionnel des pages -->
-{#if $currentPage === 'home'}
-	<HomePage />
-{:else if $currentPage === 'history'}
-	<HistoryPage />
-{:else if $currentPage === 'foods'}
-	<FoodsPage />
+{#if $currentPage === "home"}
+    <HomePage />
+{:else if $currentPage === "history"}
+    <HistoryPage />
+{:else if $currentPage === "foods"}
+    <FoodsPage />
 {/if}
